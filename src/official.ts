47d354b78f518b883f4f53d396275665f824cca1
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2025-07-09 15:08:09 +0900

    feat(official/getAlarmCnt): implement getAlarmCnt function and tests for alarm count retrieval from user
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

---
+++ src/official.ts
@@ -0,0 +1,33 @@
+import type {UserData} from "./types";
+
+async function getAlarmCnt(
+    user: UserData,
+    http: Function,
+    // 테스트 의존성 주입
+    mockShowAlarmDot?: Function,
+    $?: any
+): Promise<number> {  // 알람
+    if (Number(user.data.mem_no) === 0) throw new Error("Member not found");
+
+    const url: string = "/proc/alarm";
+    const data = {"mode": "getAlarmCnt"};
+    const http_result = await http("post", url, data);
+    const {status, result} = http_result;
+
+    if (200 !== Number(status))
+        throw new Error("status is " + status);
+
+    const {cnt = 0} = result;
+
+    if (cnt > 0) (mockShowAlarmDot || function showAlarmDot() {
+        $("#btn-alarm #pc_alarm_dot").remove();
+        $("#btn-alarm")
+            .append('<span id="pc_alarm_dot" class="red-dot"></span>');
+    })();
+
+    return cnt;
+}
+
+export {
+    getAlarmCnt
+}
