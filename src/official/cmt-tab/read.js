76de089fd02c613304a9c1544704c8e7e3cf42db
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2025-07-27 12:27:30 +0900

    fix(official/cmt-tab): fix comment fetching functions

--- src/official/cmt-tab/read.js
+++ src/official/cmt-tab/read.js
@@ -1,16 +1,11 @@
 function get_comment_in_writer() {
-    _dev.log('get_comment_in_writer');
+    console.log('get_comment_in_writer');
     $.ajax({
-        url: "/proc/user",
-        data: {
-            "mode": "get_comment_in_writer",
-            "mem_no": g_cate
-        },
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            if (response.status != '200') {
-                if (response.status == '401') {
+        url: "/proc/user", data: {
+            "mode": "get_comment_in_writer", "mem_no": g_cate
+        }, type: "POST", dataType: "json", success: function (response) {
+            if (response.status !== '200') {
+                if (response.status === '401') {
                     if (confirm("로그인이 필요합니다.\n로그인 하시겠습니까?")) {
                         member_login_modal_on();
                     }
@@ -19,12 +14,11 @@ function get_comment_in_writer() {
                 }
                 return;
             }
-            _dev.log(response.result);
-            if (response.result != '') {
+            console.log(response.result);
+            if (response.result !== '') {
                 g_is_writer = response.result.cnt;
             }
-        },
-        complete: function (userBlockCustom) {
+        }, complete: function () {
             get_user_comment();
         }
     });
@@ -35,12 +29,12 @@ function get_user_comment() {
         .append(_comment_html);
     g_this.find('select.select-align').html('');
     $.each(g_comment_sort, function (i, v) {
-        let _selected = (i == g_comment_selected_sort) ? ' selected' : '';
+        let _selected = (i === g_comment_selected_sort) ? ' selected' : '';
         g_this.find('select.select-align').append('<option value="' + i + '" ' + _selected + '>' + v + '</option>');
     });
 
-    if (g_mem_no == g_cate || g_mem_admin > 9) {
-        if (g_member_view.view_comment == 1) {
+    if (g_mem_no === g_cate || g_mem_admin > 9) {
+        if (g_member_view.view_comment === 1) {
             let _html = '';
             _html += '<img class="hide-option" src="' + G_CDN_DOMAIN_IMG + '/img/new/user_profile/hide_option_icon.png" alt="비공개">';
             g_this.find('.user_card_databox .list-tit p').prepend(_html);
@@ -50,10 +44,10 @@ function get_user_comment() {
 
         get_member_comment();
     } else {
-        if (g_member_view.view_comment == 1) {
+        if (g_member_view.view_comment === 1) {
             let _html = `
 					<div style="margin-top:30px;"></div><!--상단여백, 이동시 삭제-->
-					<div class="list-tit s_inv" style="justify-content: center; padding: 40px 0px; text-align: center;">
+					<div class="list-tit s_inv" style="justify-content: center; padding: 40px 0; text-align: center;">
 						<div class="hide-option-box">
 							<img class="s_inv" src="${G_CDN_DOMAIN_IMG}/img/new/user_profile/hide_option_korani.png" alt="비공개 설정">
 							<p style="margin-top: 10px; font-size: initial; font-weight: normal; color: #999;">비공개 설정 되었습니다.</p>
@@ -69,27 +63,21 @@ function get_user_comment() {
 }
 
 function get_member_comment() {
-    _dev.log('get_member_comment');
+    console.log('get_member_comment');
     g_paging.rowCount = 20;
     g_paging.sort = {
-        'date': 0,
-        'new': 1,
-        'vote': 1,
+        'date': 0, 'new': 1, 'vote': 1,
     };
     g_paging.order = g_comment_selected_sort;
     $.ajax({
-        url: "/proc/user",
-        data: {
+        url: "/proc/user", data: {
             "mode": "get_member_comment",
             "mem_no": g_cate,
             "paging": g_paging,
             "view_comment": g_member_view.view_comment,
-        },
-        type: "POST",
-        dataType: "json",
-        success: function (response) {
-            if (response.status != '200') {
-                if (response.status == '401') {
+        }, type: "POST", dataType: "json", success: function (response) {
+            if (response.status !== '200') {
+                if (response.status === '401') {
                     if (confirm("로그인이 필요합니다.\n로그인 하시겠습니까?")) {
                         member_login_modal_on();
                     }
@@ -98,10 +86,10 @@ function get_member_comment() {
                 }
                 return;
             }
-            _dev.log(response.result);
+            console.log(response.result);
             const novel_view_comment = Number(g_member_view.view_comment);
-            if (novel_view_comment != 0) {
-                if (g_mem_admin < 10 && g_mem_no != g_cate) {
+            if (novel_view_comment !== 0) {
+                if (g_mem_admin < 10 && g_mem_no !== g_cate) {
                     return 0;
                 }
             }
@@ -123,11 +111,11 @@ function get_member_comment() {
 					`;
 
                 $.each(response.result.novel, function (i, v) {
-                    _dev.log("comment = " + v.temp);
+                    console.log("comment = " + v.temp);
                     g_this.find('.comment-list-wp').append('<div class="comment-list" data-id="' + v.novel_no + '" data-episode="' + v.content_no + '" data-comment="' + v.comment_idx + '" data-age="' + v.novel_age + '">' + _html + '</div>');
                     let _this = g_this.find('.comment-list[data-comment="' + v.comment_idx + '"]');
 
-                    if (v.comment_status == 0) {
+                    if (v.comment_status === 0) {
                         _this.css('background-color', '#fee');
                         _this.find('.comment-area').append('<div style="color: #999;">삭제된 댓글입니다.</div>');
                         _this.find('.menu-more').remove();
@@ -135,7 +123,7 @@ function get_member_comment() {
                     if (v.novel_age >= 19) {
                         _this.find('.novel-thumbnail').prepend('<div class="adult19 s_inv"></div>');
                     }
-                    if ($.cookie("secret_mode") == '1') {
+                    if ($.cookie("secret_mode") === '1') {
                         _this.find('.menu-more').remove();
                     }
 
@@ -143,27 +131,27 @@ function get_member_comment() {
                     let _novel_thumb = v.novel_thumb;
 
                     // 전연령
-                    if (g_mem_adt_mode == 0) {
+                    if (g_mem_adt_mode === 0) {
                         _novel_img = v.novel_img_all;
                         _novel_thumb = v.novel_thumb_all;
                     }
 
                     // 성인표지가 비었을 경우
-                    if (g_mem_adt_mode == 1 && (_novel_thumb == '' || _novel_thumb == null) && v.novel_thumb_all != '' && v.novel_thumb_all != null) {
+                    if (g_mem_adt_mode === 1 && (_novel_thumb === '' || _novel_thumb == null) && v.novel_thumb_all !== '' && v.novel_thumb_all != null) {
                         _novel_img = v.novel_img_all;
                         _novel_thumb = v.novel_thumb_all;
                     }
 
                     // 전연령 소설일 경우
-                    if (g_mem_adt_mode == 1 && $.inArray(v.novel_age, ['19', '22']) == -1 && v.novel_thumb_all != '' && v.novel_thumb_all != null) {
+                    if (g_mem_adt_mode === 1 && $.inArray(v.novel_age, ['19', '22']) === -1 && v.novel_thumb_all !== '' && v.novel_thumb_all != null) {
                         _novel_img = v.novel_img_all;
                         _novel_thumb = v.novel_thumb_all;
                     }
 
-                    let _cover_src = '';
-                    if ($.cookie('COVER_VIEW') == 'OFF') {
+                    let _cover_src;
+                    if ($.cookie('COVER_VIEW') === 'OFF') {
                         _cover_src = G_CDN_DOMAIN_IMG + '/img/layout/block_01.png';
-                    } else if (_novel_thumb == '' || _novel_thumb == null || _novel_thumb == 'undefined') {
+                    } else if (_novel_thumb === '' || _novel_thumb == null || _novel_thumb === 'undefined') {
                         _cover_src = G_CDN_DOMAIN_IMG + '/img/layout/readycover4.png';
                     } else if (_novel_thumb.indexOf('/cover/') > 0) {
                         _cover_src = G_CDN_DOMAIN_IMG + _novel_thumb.replace('.file', '.thumb').replace('/img/', '/imagebox/');
@@ -180,7 +168,7 @@ function get_member_comment() {
                     _this.find('.date-recommend .recommend').text(v.user_count_good.format());
                     _this.find('.menu-more').data('id', v.novel_no);
 
-                    if (v.comment_status == 0 && v.mem_no != g_mem_no) {
+                    if (v.comment_status === 0 && v.mem_no !== g_mem_no) {
                         if (g_mem_admin < 10) {
                             _this.find('.menu-more').remove();
                             _this.find('.comment_img').remove();
@@ -199,8 +187,7 @@ function get_member_comment() {
 					`;
                 g_this.find('.comment-list-wp').html('').append(_html);
             }
-        },
-        complete: function (userBlockCustom) {
+        }, complete: function () {
             paging(g_paging, '<img src="//images.novelpia.com/img/new/user_profile/left.png">', '<img src="//images.novelpia.com/img/new/user_profile/right.png">', 'pagination', 'comment'); // 페이징
         }
     });
