852321dc144cb27a08c14009065fe1f6e4a85c39
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2025-07-27 12:28:41 +0900

    fix(official/cmt-tab): fix comment delete function

--- src/official/cmt-tab/delete.js
+++ src/official/cmt-tab/delete.js
@@ -1,42 +1,39 @@
-// 댓글/대댓글 삭제
+// 댓글/대댓글 삭제 함수
 function comment_delete_proc(comment_no) {
-    //console.log("comment_delete_proc", comment_no);
+    const is_delete_ongoing = g_comment_delete === 1;
+    const is_delete_refused = !confirm('정말 댓글을 삭제하시겠습니까?');
 
-    if (g_comment_delete == 0) {
-        if (confirm('정말 댓글을 삭제하시겠습니까?')) {
-            g_comment_delete = 1;
-            $.ajax({
-                url: "/proc/viewer",
-                data: {
-                    "mode": "set_delete_comment",
-                    "comment_no": comment_no,
-                    "csrf": "696c348ebddc22f5607bfa4bcacf29b4"
-                },
-                type: "POST",
-                dataType: "json",
-                success: function (response) {
-                    if (response.status == '200') {
-                        //console.log(response.result);
-                        alert("댓글이 삭제되었습니다.");
-                        g_comment_cnt = 0; // count init
-                        //$('.more_comment'+g_page).remove();
-                        //get_comment_load();
-                        tab_link_on();
-                    } else if (response.status == '401') {
-                        if (confirm("로그인이 필요합니다.\n로그인 하시겠습니까?")) {
-                            //location = "/page/login";
-                            //$.cookie('_redirectrurl', 'L3VzZXIvMjUyOTcyMC9jb21tZW50', { expires: 365, path: '/', domain: 'novelpia.com', secure: false });
-                            //location = "/?login_req=1";
-                            member_login_modal_on();
-                        }
-                    } else {
-                        alert(response.errmsg);
-                    }
-                },
-                complete: function () {
-                    g_comment_delete = 0;
-                }
-            });
-        }
+    if (is_delete_ongoing || is_delete_refused) {
+        return;
     }
+
+    g_comment_delete = 1; // 중복 삭제 방지 플래그 설정
+
+    $.ajax({
+        url: "/proc/viewer", data: {
+            comment_no: comment_no,
+            csrf: "(CSRF 토큰)",
+            mode: "set_delete_comment"
+        }, type: "POST", dataType: "json", success: res => {
+            switch (res.status) {
+                case "200":
+                    // 댓글 삭제 성공
+                    alert("댓글이 삭제되었습니다.");
+                    g_comment_cnt = 0; // count init
+                    tab_link_on(); // 댓글 목록 새로고침
+                    break;
+                case "401":
+                    // 댓글 삭제 실패, 로그인 필요
+                    if (confirm("로그인이 필요합니다.\n로그인하시겠습니까?")) {
+                        member_login_modal_on();
+                    }
+                    break;
+                default:
+                    alert(res.errmsg);
+                    break;
+            }
+        }, complete: () => {
+            g_comment_delete = 0; // 중복 삭제 방지 플래그 해제
+        }
+    });
 }
